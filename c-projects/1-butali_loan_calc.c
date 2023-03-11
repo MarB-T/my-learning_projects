d86dce82515ddfe2a21e326e83fa3622f210c6a5
MarB-T <danmarbet@gmail.com>
2023-03-11 23:05:14 +0300

    small changes;

--- c-projects/1-butali_loan_calc.c
+++ c-projects/1-butali_loan_calc.c
@@ -18,8 +18,9 @@ int main(void)
 	printf("Enter monthly rate: ");
 	scanf("%f", &rate);
 	
-	interest = ((principle * rate * time) + 1) / 200;
-	perc_interest = interest / principle * 100;
+	interest = (((principle * rate * time) + 1) / 200) + (principle * 0.02);
+	perc_interest = interest / (principle * 0.98) * 100;
+	printf("\nThis calculation counts the 2-PERCENT insurance deducted as interest\n");
 	printf("Percentage interest is: %.2f\n", perc_interest);
 	printf("Total interes is: %.2f\n", interest);
 	printf("Annualised interest is: %.2f\n", (perc_interest * 12 / time));
